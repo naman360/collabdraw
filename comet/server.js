b53df6ee3a98316c97e15b9f93e3ec00336e8225
Naman Shankhydhar <naman.geek@gmail.com>
2023-07-24 21:59:58 +0530

    feat(comet): socket io server setup

--- comet/server.js
+++ comet/server.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer);
 const port = process.env.PORT || 5002;
 
-app.listen(port, () => {
+io.on("connection", (socket) => {
+    console.log("User Online");
+
+    socket.on("canvas-data", (data) => {
+        socket.broadcast.emit("canvas-data", data);
+    });
+});
+
+httpServer.listen(port, () => {
     console.log(`Server is running on Port ${port}`);
 });
