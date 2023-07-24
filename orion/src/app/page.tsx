0dfb2d7f74e76b55e6f2d0af8f2cf08d33bc646b
Naman Shankhydhar <naman.geek@gmail.com>
2023-07-25 00:00:52 +0530

    feat(orion): changed socket ref variable to state

--- orion/src/app/page.tsx
+++ orion/src/app/page.tsx
@@ -1,25 +1,22 @@
 "use client";
 import Canvas from "@/components/Canvas";
-import { FC, useEffect, useRef } from "react";
+import { FC, useEffect, useState } from "react";
 import { Socket, io } from "socket.io-client";
 
 const Home: FC = () => {
-    const socketRef = useRef<Socket | null>(null);
+    const [socketState, setSocketState] = useState<Socket | null>(null);
     useEffect(() => {
-        if (!socketRef.current?.connected)
-            socketRef.current = io("http://localhost:5002");
+        if (!socketState?.connected)
+            setSocketState(io("http://localhost:5002"));
     }, []);
+
     return (
         <div className="h-screen w-full flex flex-column">
             <div className="flex items-center bg-slate-900">
                 <input type="color" />
             </div>
             <div className="w-full flex items-center justify-center">
-                <Canvas
-                    socketRef={socketRef.current}
-                    width={700}
-                    height={500}
-                />
+                <Canvas socketRef={socketState} width={700} height={500} />
             </div>
         </div>
     );
